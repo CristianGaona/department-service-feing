293d2382f0081bef8ee1327172f0015d5fbd19aa
Cristian <crgaonas24@gmail.com>
2020-07-07 15:55:41 -0500

    Comunicación de microservicios MS-CONSUMIDOR

---
+++ src/main/java/com/cdgaona/example/client/EmployeeClient.java
@@ -0,0 +1,16 @@
+package com.cdgaona.example.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cdgaona.example.model.Employee;
+
+@FeignClient(name = "employee-service")
+public interface EmployeeClient {
+	
+	@GetMapping("/department/{departmentId}")
+	List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+}
